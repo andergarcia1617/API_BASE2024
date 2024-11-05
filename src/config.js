888f0d54a3b944fa399b11a98b5903caf898091f
andergarcia1617 <andergarcia1213@gmail.com>
2024-11-05 10:11:51 -0500

    Subida correcion de JWT

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 import { config } from 'dotenv';
 
+// Cargar el archivo .env
 config();
 
+// Exportar las variables de entorno
 export const BD_HOST = process.env.BD_HOST || 'localhost';
 export const BD_DATABASE = process.env.BD_DATABASE || 'base20242';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
-export const JWT_SECRET = process.env.JWT_SECRET || '192021';  
+export const JWT_SECRET = process.env.JWT_SECRET || '192021';
